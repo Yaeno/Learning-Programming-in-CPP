21dedfffc6a04ff864d45e91427f43a93733c68a
Yaeno <ing.zhang@gmail.com>
2023-04-03 07:35:29 -0700

    hellowWordAndVariable

---
+++ IntroductionToCPP.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+// ----hello world----//
+
+/*int main() {
+
+    cout << "Hello C++ " << endl;
+
+    //system("pause") -only works for Windows
+
+
+    return 0;
+}*/
+
+int main(){
+    //变量的定义
+    //语法：数据类型 变量名 = 初始值
+
+    int a = 10;
+    cout << "a =" << a << endl;
+
+    return 0;
+}
+

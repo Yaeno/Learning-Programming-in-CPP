c9ca7193e25a7cb849cb55dd0283c29418e90875
Yaeno <ing.zhang@gmail.com>
2023-04-03 12:56:22 -0700

    1.3-1.4

--- IntroductionToCPP.cpp
+++ IntroductionToCPP.cpp
@@ -13,6 +13,9 @@ using namespace std;
     return 0;
 }*/
 
+
+//----1.3 Variable ----//
+/*
 int main(){
     //变量的定义
     //语法：数据类型 变量名 = 初始值
@@ -21,5 +24,27 @@ int main(){
     cout << "a =" << a << endl;
 
     return 0;
-}
+}*/
 
+
+//----1.4 Constant----//
+// 1. #define 
+// 2. const
+
+// ex1. Macro Constant
+/*#define Day 7
+
+int main(){
+
+    cout <<"One week has " << Day << " days." <<endl;
+
+    // 2. const decorative constant
+    const int month = 12;
+    //month = 24; Error
+
+    cout << "There are "<< month << " months in a year." <<endl;
+
+    return 0;
+}*/
+
+// ---- 1.5 Key Word 关键字 ----

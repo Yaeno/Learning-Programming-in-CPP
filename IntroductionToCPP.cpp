eddd2b86438b6be96e9a1b41bebb84a6bb71d17f
Yaeno <ing.zhang@gmail.com>
2023-04-03 22:52:37 -0700

    till 2.3

--- IntroductionToCPP.cpp
+++ IntroductionToCPP.cpp
@@ -47,4 +47,44 @@ int main(){
     return 0;
 }*/
 
-// ---- 1.5 Key Word 关键字 ----
+// ======== 2. Data Type ==========
+//----2.2 sizeof ----//
+/*int main(){
+
+    cout << "short 类型所占内存空间为： " << sizeof(short) << endl;
+
+    cout << "int 类型所占内存空间为： " << sizeof(int) << endl;
+
+    cout << "long 类型所占内存空间为： " << sizeof(long) << endl;
+
+    cout << "long long 类型所占内存空间为： " << sizeof(long long) << endl;
+
+    return 0;
+
+}*/
+
+//----2.3 Floats ----//
+int main(){
+    float f1 = 3.14f; /*在数字后面加f，强调这个值是float*/
+    double d1 = 3.14;
+
+    cout << f1 << endl;
+    cout << d1 << endl;
+
+    cout << "float sizeOf = " << sizeof(f1) << endl;
+    cout << "double sizeOf = " << sizeof(d1) << endl;
+
+    //科学计数法
+    float f2 = 3e2; // 3 * 10 ^ 2
+    cout << "f2 = " << f2 << endl;
+
+    float f3 = 3e-2;  // 3 * 0.1 ^ 2
+    cout << "f3 = " << f3 << endl;
+
+
+    return 0;
+}
+
+
+
+

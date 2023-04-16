ac5b9b109e1fa0e61d5f855a9ca4017a7c75bdea
Yaeno <ing.zhang@gmail.com>
2023-04-16 09:45:34 -0700

    Updates

--- IntroductionToCPP.cpp
+++ IntroductionToCPP.cpp
@@ -64,8 +64,8 @@ int main(){
 }*/
 
 //----2.3 Floats ----//
-int main(){
-    float f1 = 3.14f; /*在数字后面加f，强调这个值是float*/
+/* int main(){
+    float f1 = 3.14f; //在数字后面加f，强调这个值是float//
     double d1 = 3.14;
 
     cout << f1 << endl;
@@ -74,16 +74,42 @@ int main(){
     cout << "float sizeOf = " << sizeof(f1) << endl;
     cout << "double sizeOf = " << sizeof(d1) << endl;
 
-    //科学计数法
-    float f2 = 3e2; // 3 * 10 ^ 2
+    // 科学计数法
+    float f2 = 3e2; // e(n) = 10^n
     cout << "f2 = " << f2 << endl;
 
-    float f3 = 3e-2;  // 3 * 0.1 ^ 2
+    float f3 = 3e-2; // e(-n) = 0.1^n
     cout << "f3 = " << f3 << endl;
 
+    return 0;
+} */
+
+//----2.4 Char ----//
+int main(){
+
+    // 1. Create Char variable
+    char ch = 'a';
+    cout << ch << endl;
+
+    // 2. Figure out the size of a char 
+    cout << "char sizeOf = " << sizeof(ch) << endl;
+
+    // 3. Common Mistakes
+    // mistake 1: char ch2 = "b";
+    // mistake 2: char ch2 = "xyz";
+
+
+    // 4. Mapped ASCII Code
+    // Two common ASCII cases
+    // a = 97
+    // A = 65 
+    cout << "The mapped ASCII is " << (int)ch << endl;
+    cout << "The mapped ASCII is " << (int)'A' << endl;
 
     return 0;
 }
+
+
 
 
 
